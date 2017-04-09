bbe311efbe5e7d9b6dae4a43631c31501717f767
Jun <felipe.jun.ohira@gmail.com>
2017-04-09 15:35:31 -0300

    Adding schema validation

--- app/schemas/schemas/v2/users/create_schema.rb
+++ app/schemas/schemas/v2/users/create_schema.rb
@@ -1,3 +1,20 @@
-class Schemas::V2::Users::CreateSchema
+module Schemas::V2::Users
+  CreateSchema = Dry::Validation.Schema do
+    required(:name).filled
 
+    required(:age).maybe(:int?)
+
+    required(:address).schema do
+      required(:street).filled
+      required(:city).filled
+      required(:zipcode).filled
+
+      required(:telephones).each do
+        schema do
+          required(:type).filled
+          required(:number).filled
+        end
+      end
+    end
+  end
 end
